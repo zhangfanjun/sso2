a01a634638359f358cd19f9d99546f90109f61bb
fanjun_zhang <fanjun_zhang@jinyafu.com>
2022-02-11 18:20:17 +0800

    数据保存的密码是明文，实际上需要的是密文，生产采用保存密文

--- oauther2/src/main/java/com/zhang/oauther2/service/MyUserDetailService.java
+++ oauther2/src/main/java/com/zhang/oauther2/service/MyUserDetailService.java
@@ -3,6 +3,7 @@ package com.zhang.oauther2.service;
 import com.zhang.oauther2.model.UserInfo;
 import com.zhang.oauther2.mysql.service.ISsoPermissionRoleService;
 import com.zhang.oauther2.mysql.service.ISsoUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -16,6 +17,7 @@ import org.springframework.util.CollectionUtils;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class MyUserDetailService implements UserDetailsService {
 
@@ -31,13 +33,15 @@ public class MyUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserInfo userInfo = userService.getUserInfoByUserName(username);
+        log.info("用户信息：{}",userInfo);
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         if (!CollectionUtils.isEmpty(userInfo.getRolesList())) {
             userInfo.getRolesList().forEach(role ->
                     authorities.add(new SimpleGrantedAuthority(role.getCode()))
             );
         }
-        return new User(username, userInfo.getPassword(), userInfo.isEnabled(),
+
+        return new User(username, this.passwordEncoder.encode(userInfo.getPassword()), userInfo.isEnabled(),
                 userInfo.isAccountNonExpired(), userInfo.isCredentialsNonExpired(),
                 userInfo.isAccountNonLocked(), authorities);
     }
